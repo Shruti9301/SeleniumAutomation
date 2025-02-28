36715056f7c15d0f952f45e5de86531d8a856ed3
Anshul Chourasiya <Anshul Chourasiya@DESKTOP-3J82O7A>
2025-02-28 22:16:16 +0530

    Hey

--- src/test/java/com/crm/PageObject/Login.java
+++ src/test/java/com/crm/PageObject/Login.java
@@ -20,7 +20,7 @@ public class Login {
 		dws.username.sendKeys("admin01@gmail.com");
 		dws.password.sendKeys("admin01");
 		dws.rememberMe.click();
-		dws.login_in.click();
+		dws.login_button.click();
 		driver.close();
 		
 	}
